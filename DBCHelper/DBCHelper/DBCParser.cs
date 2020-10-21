867be239bfb8a062427f45f5ef913ee78d8c5db5
DESKTOP-KLGCS5E\axlov <axlover@hotmail.com>
2020-10-21 16:35:25 +0200

    * parser init * signal object init

---
+++ DBCHelper/DBCHelper/DBCParser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DBCHelper
+{
+
+    public class DBCParser
+    {
+        #region Private Field
+
+        private string mPath;
+
+        #endregion
+
+        public DBCParser()
+        {
+            mPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}";
+        }
+
+        public void LoadFile()
+        {
+            using FileStream fileStream = new FileStream(mPath, FileMode.Append, FileAccess.Read, FileShare.ReadWrite);
+            using StreamReader streamReader = new StreamReader(fileStream);
+
+            streamReader.ReadToEnd();
+
+            
+
+        }
+
+
+    }
+}
---
+++ DBCHelper/DBCHelper/SignalCAN.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBCHelper
+{
+    public enum EByteOrder
+    {
+        Intel,
+        Motorola
+    }
+
+    public enum EValueType
+    {
+        SignedType,
+        UnsignedType
+    }
+
+    public enum EVariableType
+    {
+        IntegerType,
+        FloatType,
+        StringType
+    }
+
+    public enum EAccessType
+    {
+        Unrestricted,
+        Read,
+        Write,
+        ReadWrite
+    }
+
+    public class SignalCAN
+    {
+        public string NetworkNode { get; set; }
+        public string SignalName { get; set; }
+        
+        public string MessageName { get; set; }
+        public uint ID { get; set; }
+
+        public int StartBit { get; set; }
+
+        public int Length { get; set; }
+
+        public EByteOrder ByteOrder { get; set; }
+
+        public EValueType ValueType { get; set; }
+
+        public double Maximum { get; set;}
+
+        public double Minimum { get; set;}
+
+        public double InitialValue { get; set; }
+
+        public string Unit { get; set; }
+
+        public string Comment { get; set; }
+
+
+
+    }
+}
