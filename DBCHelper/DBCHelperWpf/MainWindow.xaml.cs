59d961f0defe55811cd998c213a5bee3369ecefb
DESKTOP-KLGCS5E\axlov <axlover@hotmail.com>
2020-11-15 22:23:08 +0100

    - design template init

--- DBCHelper/DBCHelperWpf/MainWindow.xaml.cs
+++ DBCHelper/DBCHelperWpf/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BeautySolutions.View.ViewModel;
+using DropDownMenu;
+using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +26,45 @@ namespace DBCHelperWpf
         public MainWindow()
         {
             InitializeComponent();
+
+            var menuRegister = new List<SubItem>();
+            menuRegister.Add(new SubItem("Customer"));
+            menuRegister.Add(new SubItem("Providers"));
+            menuRegister.Add(new SubItem("Employees"));
+            menuRegister.Add(new SubItem("Products"));
+            var item6 = new ItemMenu("Register", menuRegister, PackIconKind.Register);
+
+            var menuSchedule = new List<SubItem>();
+            menuSchedule.Add(new SubItem("Services"));
+            menuSchedule.Add(new SubItem("Meetings"));
+            var item1 = new ItemMenu("Appointments", menuSchedule, PackIconKind.Schedule);
+
+            var menuReports = new List<SubItem>();
+            menuReports.Add(new SubItem("Customers"));
+            menuReports.Add(new SubItem("Providers"));
+            menuReports.Add(new SubItem("Products"));
+            menuReports.Add(new SubItem("Stock"));
+            menuReports.Add(new SubItem("Sales"));
+            var item2 = new ItemMenu("Reports", menuReports, PackIconKind.FileReport);
+
+            var menuExpenses = new List<SubItem>();
+            menuExpenses.Add(new SubItem("Fixed"));
+            menuExpenses.Add(new SubItem("Variable"));
+            var item3 = new ItemMenu("Expenses", menuExpenses, PackIconKind.ShoppingBasket);
+
+            var menuFinancial = new List<SubItem>();
+            menuFinancial.Add(new SubItem("Cash flow"));
+            var item4 = new ItemMenu("Financial", menuFinancial, PackIconKind.ScaleBalance);
+
+            var item0 = new ItemMenu("Dashboard", new UserControl(), PackIconKind.ViewDashboard);
+
+            Menu.Children.Add(new UserControlMenuItem(item0));
+            Menu.Children.Add(new UserControlMenuItem(item6));
+            Menu.Children.Add(new UserControlMenuItem(item1));
+            Menu.Children.Add(new UserControlMenuItem(item2));
+            Menu.Children.Add(new UserControlMenuItem(item3));
+            Menu.Children.Add(new UserControlMenuItem(item4));
+
         }
     }
 }
