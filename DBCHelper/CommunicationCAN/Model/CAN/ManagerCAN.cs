2936c10d818fb34dbc020975a19547affec55434
Seokho Youn <axlover@hotmail.com>
2020-12-06 17:18:08 +0900

    managing can class added

---
+++ DBCHelper/CommunicationCAN/Model/CAN/ManagerCAN.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunicationCAN.Model.CAN
+{
+    public enum ECANType
+    {
+        Peak,
+        Vector
+    }
+
+    public class ManagerCAN
+    {
+
+        public ManagerCAN(ECANType type )
+        {
+
+        }
+
+    }
+}
