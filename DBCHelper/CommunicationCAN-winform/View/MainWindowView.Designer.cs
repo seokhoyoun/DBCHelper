62a6264800ff15edb6e831511ebb01983ff6c515
DESKTOP-KLGCS5E\axlov <axlover@hotmail.com>
2021-01-04 17:55:36 +0900

    - winform porting...

---
+++ DBCHelper/CommunicationCAN-winform/View/MainWindowView.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace CommunicationCAN_winform
+{
+    partial class MainWindowView
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 478);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(139, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(984, 478);
+            this.panel1.TabIndex = 2;
+            // 
+            // MainWindowView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1135, 502);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listView1);
+            this.Name = "MainWindowView";
+            this.Text = "CommunicationCAN";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ DBCHelper/CommunicationCAN-winform/View/MainWindowView.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommunicationCAN_winform
+{
+    public partial class MainWindowView : Form
+    {
+        #region Public Properties
+
+        public List<string> NodeStringList
+        {
+            get;
+            private set;
+        } = new List<string>();
+
+        #endregion
+
+        #region Constructor
+
+        public MainWindowView()
+        {
+            InitializeComponent();
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void InitializeNodeListView()
+        {
+            NodeStringList.Add("A1");
+            NodeStringList.Add("A2");
+            NodeStringList.Add("A3");
+            NodeStringList.Add("A4");
+            NodeStringList.Add("A5");
+
+        }
+
+        #endregion
+
+
+
+
+    }
+}
